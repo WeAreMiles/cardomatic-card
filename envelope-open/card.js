fed5bac89593689589d288e84cefcc08fe8d3a31
Miles <miles@wearetilt.com>
2015-06-10 12:19:53 +0100

    integrated into one.

--- envelope-open/card.js
+++ envelope-open/card.js
@@ -11,65 +11,90 @@
       back2 = getID('back2'),
       timer = null;
 
-  // console.log('wat', card);
+
   next1.addEventListener('click', function () {
     cardWrapper.setAttribute('class', 'move1');
     card.setAttribute('class', 'open-half');
+
     if (timer) clearTimeout(timer);
+
     timer = setTimeout(function () {
+
       card.setAttribute('class', 'open-fully');
-      timer = null;
       next1.setAttribute('class', 'hide');
       back1.setAttribute('class', '');
       next2.setAttribute('class', '');
       back2.setAttribute('class', 'hide');
 
+      timer = null;
+
     }, 200);
+
     return false;
+
   });
 
   next2.addEventListener('click', function () {
       cardWrapper.setAttribute('class', 'move2');
       card.setAttribute('class', 'open-half2');
+
       if (timer) clearTimeout(timer);
+
       timer = setTimeout(function () {
+
           card.setAttribute('class', 'open-fully2');
           next2.setAttribute('class', 'hide');
           next1.setAttribute('class', 'hide');
           back1.setAttribute('class', 'hide');
           back2.setAttribute('class', '');
+
           time = null;
+
       }, 200);
+
       return false;
+
   })
 
   back1.addEventListener('click', function () {
     cardWrapper.setAttribute('class', '');
     card.setAttribute('class', 'close-half');
+
     if (timer) clearTimeout(timer);
+
     timer = setTimeout(function () {
+
       card.setAttribute('class', '');
-      timer = null;
       next1.setAttribute('class', '');
       next2.setAttribute('class', 'hide');
       back1.setAttribute('class', 'hide');
       back2.setAttribute('class', 'hide');
+
+      timer = null;
+
     }, 200);
+
     return false;
+
   });
 
   back2.addEventListener('click', function (){
       cardWrapper.setAttribute('class', 'move1');
       if (timer) clearTimeout(timer);
       timer = setTimeout(function () {
+
         card.setAttribute('class', 'open-fully');
-        timer = null;
         next1.setAttribute('class', 'hide');
         back1.setAttribute('class', '');
         next2.setAttribute('class', '');
         back2.setAttribute('class', 'hide');
+
+        timer = null;
+
       }, 200);
+
       return false;
+      
   })
 
 
